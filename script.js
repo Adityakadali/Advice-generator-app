6c31de149cf20664b57b98ddfdeec782ff6197dc
Aditya <adityakadali00@gmail.com>
2022-07-15 23:11:52 +0530

    Function invoking on pageload

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const url = "https://api.adviceslip.com/advice";
 var idTag = document.querySelector(".advice-id");
 var quote = document.querySelector(".quote");
-
 var dice = document.querySelector(".circle");
 
-dice.addEventListener("click", (event) => {
+function LoadAdvice() {
   fetch(url)
     .then((response) => response.json())
     .then((object) => {
       idTag.innerHTML = `ADVICE #${object.slip.id}`;
       quote.innerHTML = `${object.slip.advice}`;
       console.log(object);
-    });
-});
+    })
+    .catch();
+}
+
+dice.addEventListener("click", (event) => LoadAdvice());
+
+LoadAdvice()
