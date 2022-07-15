447efa2e149eccc928d4e61fcb54b9f210e616b4
Aditya <adityakadali00@gmail.com>
2022-07-15 22:59:48 +0530

    Added API Call

---
+++ script.js
@@ -0,0 +1,15 @@
+const url = "https://api.adviceslip.com/advice";
+const idTag = document.querySelector(".advice-id");
+var quote = document.querySelector(".quote");
+
+var dice = document.querySelector(".circle");
+
+dice.addEventListener("click", (event) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((object) => {
+      idTag.innerHTML = `ADVICE #${object.slip.id}`;
+      quote.innerHTML = `${object.slip.advice}`;
+      console.log(object);
+    });
+});
